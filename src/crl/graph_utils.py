3b063574a1adf385dd19883aa0a21699fbee8a60
Alex Inch <ucabaj7@ucl.ac.uk>
2025-06-17 02:28:29 +0100

    start package

---
+++ src/crl/graph_utils.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+# def
+
+
+def despine(ax: Axes) -> None:
+    for side in ("top", "right"):
+        ax.spines[side].set_visible(False)
