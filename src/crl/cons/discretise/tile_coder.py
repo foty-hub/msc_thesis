ff7c5fba790c2f01ed15689083071dde66d1406c
Alex Inch <ucabaj7@ucl.ac.uk>
2025-08-12 01:44:18 +0100

    add discretise function for the tile coder

--- src/crl/cons/discretise/tile_coder.py
+++ src/crl/cons/discretise/tile_coder.py
@@ -1,12 +1,15 @@
+import numpy as np
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv
 from stable_baselines3.dqn import DQN
 from PyFixedReps import TileCoder, TileCoderConfig
 from crl.cons.discretise.grid import run_test_episodes, compute_bin_ranges
 
 
+# %%
 def build_tile_coding(model: DQN, vec_env: VecEnv, tiles: int, tilings: int):
     stats = run_test_episodes(model, vec_env)
     dims = vec_env.observation_space.shape[0]
+    n_actions = vec_env.action_space.n
 
     state_bins = [tiles] * dims
 
@@ -23,4 +26,17 @@ def build_tile_coding(model: DQN, vec_env: VecEnv, tiles: int, tilings: int):
         input_ranges=input_ranges,
     )
     tc = TileCoder(config=cfg)
-    return (tc.encode, tc.features())
+
+    n_state_features = tc.features()
+
+    def discretise(obs: np.array, action: np.array):
+        # the observation and array come with a spurious batch dimension
+        state_vals = tc.get_indices(obs[0])
+        # offset so there's a unique id for each action and state pair.
+        state_vals = action * n_state_features + state_vals
+        return state_vals[0]  # TODO: tidy this up to handle multiple output ids
+
+    return discretise, n_state_features * n_actions
+
+
+# %%
