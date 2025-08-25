d03ea3f48e49481922c6409c3d8b072c9bee9d24
Alex Inch <ucabaj7@ucl.ac.uk>
2025-08-25 02:47:22 +0100

    refactoring

--- src/crl/cons/_type.py
+++ src/crl/cons/_type.py
@@ -3,3 +3,6 @@ from typing import Literal
 ClassicControl = Literal[
     "CartPole-v1", "Acrobot-v1", "LunarLander-v3", "MountainCar-v0"
 ]
+
+AgentTypes = Literal["vanilla", "cql", "ddqn"]
+ScoringMethod = Literal["monte_carlo", "td"]
